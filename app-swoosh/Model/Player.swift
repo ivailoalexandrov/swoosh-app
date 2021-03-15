007d91c8d8ccd2b5a9e0cda7f1f75be1c7e6d1ad
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2021-03-16 00:17:03 +0200

    IBActions on iOS

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ivailo Alexandrov on 15.03.21.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}

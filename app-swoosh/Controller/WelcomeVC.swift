ba770ffd06ed3c22c9b6937e361436e322c2783e
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2021-03-15 23:16:23 +0200

    removeOutlets

--- app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/Controller/WelcomeVC.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgimg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
